e2cb3ac0a27a68ef8b93c2ff50c90cba33029e36
John Darlington <onuohadarlington38@gmail.com>
2025-08-28 07:21:59 +0100

    boost seo 3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,42 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-	siteUrl: process.env.SITE_URL || 'https://blog.wefithost.com',
-	generateRobotsTxt: true, // Automatically generates robots.txt
-	sitemapSize: 5000, // (optional) split sitemaps if huge site
-	changefreq: 'daily',
-	priority: 1,
-	exclude: ['/admin/*'], // Pages or patterns to skip
-	robotsTxtOptions: {
-		policies: [{ userAgent: '*', allow: '/', disallow: ['/admin'] }],
+const config = {
+	siteUrl: 'https://blog.wefithost.com', // your domain
+	generateRobotsTxt: true, // also generate robots.txt
+	changefreq: 'weekly',
+	priority: 0.7,
+	sitemapSize: 5000,
+
+	additionalPaths: async (config) => {
+		// Connect to MongoDB
+		const { default: connectMongo } = await import('./lib/connect-mongo');
+		const { default: Article } = await import('./lib/models/article');
+
+		await connectMongo();
+
+		// Fetch all published articles
+		const articles = await Article.find({ published: true })
+			.populate({ path: 'topic', select: 'slug' })
+			.lean();
+
+		// Build unique topic list (just slugs)
+		const uniqueTopics = [...new Set(articles.map((a) => a.topic.slug))];
+
+		// Map articles to sitemap entries
+		const articlePaths = articles.map((article) => ({
+			loc: `/topics/${article.topic.slug}/${article.slug}`,
+			changefreq: 'weekly',
+			priority: 0.8,
+		}));
+
+		// Map topic pages to sitemap entries
+		const topicPaths = uniqueTopics.map((topicSlug) => ({
+			loc: `/topics/${topicSlug}`,
+			changefreq: 'weekly',
+			priority: 0.6,
+		}));
+
+		return [...articlePaths, ...topicPaths];
 	},
 };
+
+export default config;
