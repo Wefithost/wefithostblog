9fe0d2ad59052e4afbd6f51e9831b5901ebb4c71
John Darlington <onuohadarlington38@gmail.com>
2026-04-17 16:19:06 +0100

    PAY AND I'LL REDEPLOY THE BLOG :)...

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+			<h1 className="text-8xl font-bold text-red-500 max-w-3xl text-center">
+				PAY AND I'LL REDEPLOY THE BLOG :)...
+			</h1>
+		</div>
+	);
+}
+
